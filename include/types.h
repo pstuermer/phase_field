c7daacdd45350896462e898c2bb27665e9dc1a72
Philipp Sturmer <philst@gp4.matfys.lth.se>
2025-11-10 19:38:32 +0100

    fix typos in types.h

--- include/types.h
+++ include/types.h
@@ -1,15 +1,16 @@
-#ifnded TYPES_H
+#ifndef TYPES_H
 #define TYPES_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <complex.h>
 #include <stdint.h>
 
 typedef float f32;
 typedef double f64;
 typedef complex c32;
-typedef complex double f64;
+typedef complex double c64;
 
 #endif //TYPES_H

e846708d8469e156c462a7ef3a431e8b1af16f8b
Philipp Sturmer <philst@endever.matfys.local>
2025-11-10 16:13:46 +0100

    added destructor, time setters and cahn_hilliard specific functions to equation.c

--- src/equation.c
+++ src/equation.c
@@ -90,4 +90,77 @@ equation_t *create_cahn_hilliard(uint64_t dim, uint64_t *N,
   return eq;
 }
 
+/* ------------------------------------------------------------------ */
+void equation_destroy_internal(equation_t **eq) {
+  if (NULL == eq || NULL == *eq) return;
+
+  if ((*eq)->ftable && (*eq)->ftable->cleanup) {
+    (*eq)->ftable->cleanup(*eq);
+  }
+
+  grid_destroy((*eq)->grid);
+  safe_free( *eq );
+}
+
+static void cahn_hilliard_cleanup(equation_t *eq) {
+  cahn_hilliard_data_t *data = (cahn_hilliard_data_t*)eq->data;
+
+  if (data) {
+    safe_free( data->c );
+    safe_free( data->wrk1 );
+    safe_free( data->wrk2 );
+
+    safe_free( data->linear_op );
+
+    if (data->fwd_plan) fftw_destroy_plan( fwd_plan );
+    if (data->bwd_plan) fftw_destroy_plan( bwd_plan );
+
+    safe_free( data );
+  }
+}
+
+/* ------------------------------------------------------------------ */
+void set_nonlinear_cahn_hilliard(equation_t *eq,
+				 f64 (*nonlinear)(f64 *params)) {
+  if (eq->type != EQUATION_CAHN_HILLIARD) {
+    fprintf(stderr, ANSI_COLOR_RED "Error" ANSI_COLOR_RESET 
+            ": Equation is not Cahn-Hilliard type\n");
+    return;
+  }
+  
+  cahn_hilliard_data_t *data = (cahn_hilliard_data_t*)eq->data;
+  data->nonlinear = nonlinear;
+}
+
+void set_mu_cahn_hilliard(equation_t *eq, f64 (*mu)(f64 *params)) {
+  if (eq->type != EQUATION_CAHN_HILLIARD) {
+    fprintf(stderr, ANSI_COLOR_RED "Error" ANSI_COLOR_RESET 
+            ": Equation is not Cahn-Hilliard type\n");
+    return;
+  }
+  
+  cahn_hilliard_data_t *data = (cahn_hilliard_data_t*)eq->data;
+  data->mu = mu;
+}
+
+
+/* ------------------------------------------------------------------ */
+void set_iter(equation_t *eq, f64 dt,
+	      uint64_t max_iter, uint64_t iter) {
+  *(f64*)&eq->dt = dt;
+  *(uint64_t*)&eq->max_iter = max_iter;
+  eq->iter = iter;
+}
+
+void set_semi_implicit_prop(equation_t *eq) {
+  eq->time_method = TIME_SEMI_IMPLICIT;
+  
+  if (eq->ftable->setup_spectral) {
+    eq->ftable->setup_spectral(eq);
+  }
+}
+
+/* ------------------------------------------------------------------ */
+    
+
 
